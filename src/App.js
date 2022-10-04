bd1bd8ae02b5cc692d11f263342fed9a2132fd81
Ahmai Chaney-Smith <ahmai.chaney-smith@macaulay.cuny.edu>
2022-10-04 17:56:58 -0400

    first revision

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, {useState} from "react";
 import "./App.css";
 
-function City(props) {
-  const data = props.data;
+function App() {
+  const [data, setData] = useState([]);
+  const [zipCode, setZipCode] = useState('');
+  
   let firstData = data[0];
   let city = '';
   let state = '';
@@ -10,73 +12,18 @@ function City(props) {
   let long = '';
   let estimatedPop = '';
   let totalWages = '';
-  
+
   if(firstData !== undefined){
-    city = firstData.City;
-    state = firstData.State;
-    lat = firstData.Lat;
-    long = firstData.Long;
-    estimatedPop = firstData.EstimatedPopulation;
-    totalWages = firstData.TotalWages;
+    document.getElementById("the-info").className = "list-unstyled d-block"
+    city = firstData.City + ', ' + firstData.State;
+    state = 'State: ' + firstData.State;
+    lat = 'Location: (' + firstData.Lat + ', ';
+    long = firstData.Long + ')';
+    estimatedPop = 'Population (estimated): ' + firstData.EstimatedPopulation;
+    totalWages = 'Total Wages: ' + firstData.TotalWages;
   } else{
-    city = 'No results found';
-    state = '';
-    lat = '';
-    estimatedPop = '';
-    totalWages = '';
+    city = <b>No results found</b>;
   }
-
-  return(
-    <div>
-      <div class="container">
-        <div class="row">
-            <div class="col-12 rounded">
-              <div>
-                <pre>{city}</pre>
-              </div>
-              <div>
-                <ul>
-                  <li><pre>State: {state}</pre></li>
-                  <li><pre>Location: ({lat}, {long})</pre></li>
-                  <li><pre>Population (estimated): {estimatedPop}</pre></li>
-                  <li><pre>Total Wages: {totalWages}</pre></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-      </div>
-    </div>
-  );
-}
-
-function ZipSearchField(props) {
-  const setZipCode = props.setZipCode;
-  const fetchData = props.fetchData;
-  const data = props.data;
-  const zipCode = props.zipCode;
-
-  return(
-    <div>
-      <br></br>
-        <div class="container">
-          <div class="row">
-            <div>
-              <form>
-                <label htmlFor="zip">Zip Code:</label><br></br>
-                <input type="text" id="zip" name="zip" class="rounded w-100" onChange={(e) => setZipCode(e.target.value) & fetchData(e.target.value) & console.log("Zipcode: " + zipCode) & console.log("Data: " + data)} required pattern="[0-9]+"></input>
-                <br></br>
-              </form>
-            </div>
-          </div>
-        </div>
-      <br></br>
-    </div>
-  );
-}
-
-function App() {
-  const [data, setData] = useState([]);
-  const [zipCode, setZipCode] = useState('');
 
   const fetchData = () => {
     fetch('https://ctp-zip-api.herokuapp.com/zip/' + zipCode , {
@@ -86,23 +33,56 @@ function App() {
     }
     })
     .then((response) => response.json())
-    .then((reponse) => setData(reponse))
-    .catch((error) => console.log("NO RESULTS FOUND FOR " + zipCode));
+    .then((responseData) => setData(responseData))
+    .catch((error) => (console.log("NO RESULTS FOUND FOR " + zipCode)) & (
+      document.getElementById("city-section").innerHTML = "<b>No results found</b>",
+      document.getElementById("the-info").className = "list-unstyled d-none"
+    ));
   }
-
+  
   return (
-    <div className="App">
-      <div className="App-header">
-        <h1>Zip Code Search</h1>
-      </div>
-      <div className="mx-auto" style={{ maxWidth: 400 }}>
-        <ZipSearchField setZipCode={setZipCode} fetchData={fetchData} data={data} zipCode={zipCode}/>
-        <div>
-          <City data={data}/>
+  <div className="App">
+    <div className="App-header">
+      <h1>Zip Code Search</h1>
+    </div>
+    <br></br>
+
+    <div className="container">
+      <div className="row">
+        <div style={{marginTop: 20}}>
+          <div className="Main-box d-flex justify-content-center">
+            <form>
+              <label>Zip Code:</label>
+              <input type="text" onChange={(e) => setZipCode(e.target.value)} className="rounded" required pattern="[0-9]+"></input>
+            </form>
+          </div>
+
+          <div className="Main-box d-flex justify-content-center mt-1">
+            <button onClick={() => fetchData(zipCode)}>Search</button>
+          </div>
+          <br></br>
+
+          <div>
+              <div className="row">
+                <div className="Main-box row bg-light border border-dark rounded-top w-100">
+                  <pre id="city-section">{city}</pre>
+                </div>
+                <div className="row bg-primary border border-dark rounded-bottom w-100 text-white">
+                  <ul className="list-unstyled d-none" id="the-info">
+                    <li className="justify-content-left" id="under-city"><pre>{state}</pre></li>
+                    <li className="justify-content-left"><pre id="under-city">{lat}{long}</pre></li>
+                    <li className="justify-content-left"><pre id="under-city">{estimatedPop}</pre></li>
+                    <li className="justify-content-left"><pre id="under-city">{totalWages}</pre></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <br></br>
         </div>
       </div>
     </div>
-  );
+  </div>
+  ) 
 }
 
 export default App;
